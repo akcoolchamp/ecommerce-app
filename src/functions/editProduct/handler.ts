987854a3e197aa462d3cae43c582b48fa9138d6c
akshay <lamkhadeakshay98@gmail.com>
2024-06-08 11:20:49 +0530

    Edit product

--- src/functions/editProduct/handler.ts
+++ src/functions/editProduct/handler.ts
@@ -2,21 +2,19 @@ import { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
 import schema, { validateRequest } from "./schema";
 import { ProductService } from "@services/productService";
-import { randomUUID } from "crypto";
-import moment from "moment";
 
 const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (
     event
 ) => {
     try {
+        const productId = event.pathParameters.productId;
         const body = await validateRequest(event.body);
-        const productId = randomUUID();
-        const params = {
-            ...body,
-            productId,
-        };
+        //Check if product exists
         const productServiceObject = new ProductService();
-        const response = await productServiceObject.createProduct(params);
+        const response = await productServiceObject.updateProduct(
+            productId,
+            body
+        );
         return response;
     } catch (error) {
         console.log(error);
