c62d07540b870763f7fa2753437bcd05e71d021c
akshay <lamkhadeakshay98@gmail.com>
2024-06-08 09:55:33 +0530

    Setup of serverless

---
+++ serverless.ts
@@ -0,0 +1,48 @@
+const NODE_ENV = process.env.NODE_ENV || "dev";
+
+import type { AWS } from "@serverless/typescript";
+
+const serverlessConfiguration: AWS = {
+    service: "ecommerce-application",
+    frameworkVersion: "3",
+    plugins: [
+        "serverless-esbuild",
+        "serverless-offline",
+        "serverless-dotenv-plugin",
+    ],
+    provider: {
+        name: "aws",
+        runtime: "nodejs18.x",
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true,
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+            NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+        },
+        deploymentBucket: process.env.DEPLOYMENT_BUCKET,
+        deploymentMethod: "direct",
+        versionFunctions: false,
+    },
+    // import the function via paths
+    functions: {},
+    package: { individually: true },
+    custom: {
+        esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ["aws-sdk"],
+            target: "node14",
+            define: { "require.resolve": undefined },
+            platform: "node",
+            concurrency: 10,
+        },
+        dotenv: {
+            path: `./config/.env.${NODE_ENV}`,
+        },
+    },
+};
+
+module.exports = serverlessConfiguration;
